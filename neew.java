e84908424f20423f471c75f705b1a1caab258bfe
Hajrudin <Sehic@users.noreply.github.com>
2015-05-24 22:47:58 +0200

    Update neew.java

--- neew.java
+++ neew.java
@@ -6,31 +6,29 @@ public class neew {
     int hour;
     int minute;
     int second;
-        long totalMilliSeconds=System.currentTimeMillis();
-        long totalSeconds=totalMilliSeconds/1000;
-        second=(int)(totalSeconds%60);
-        long totalMinutes=totalSeconds/60;
-        minute=(int)(totalMinutes%60);
-        long totalHours=totalMinutes/60;
-        hour=(int)(totalHours%24);
-       
-        hour+=2;
-        System.out.println(hour);
-        if(hour>=6 && hour<10){
-        System.out.println("Morning!! Time:"+hour+":"+minute+":"+second);
+    long totalMilliSeconds=System.currentTimeMillis();
+    long totalSeconds=totalMilliSeconds/1000;
+    second=(int)(totalSeconds%60);
+    long totalMinutes=totalSeconds/60;
+    minute=(int)(totalMinutes%60);
+    long totalHours=totalMinutes/60;
+    hour=(int)(totalHours%24);
+    hour+=2;
+    if(hour>=6 && hour<10){
+    System.out.println("Morning!! Time:"+hour+":"+minute+":"+second);
+    }
+    else if(hour>=10 && hour<18){
+        System.out.println("Day!! Time:"+hour+":"+minute+":"+second);
         }
-        else if(hour>=10 && hour<18){
-            System.out.println("Day!! Time:"+hour+":"+minute+":"+second);
-            }
-        else if(hour>=18 && hour<22){
-            System.out.println("Evening!! Time:"+hour+":"+minute+":"+second);
-            }
-        else if(hour>=22){
-            System.out.println("Night!! Time:"+hour+":"+minute+":"+second);
-            }
-        else if(hour>=0 && hour<6){
-            System.out.println("Night!! Time:"+hour+":"+minute+":"+second);
-            }
+    else if(hour>=18 && hour<22){
+        System.out.println("Evening!! Time:"+hour+":"+minute+":"+second);
+        }
+    else if(hour>=22){
+        System.out.println("Night!! Time:"+hour+":"+minute+":"+second);
+        }
+    else if(hour>=0 && hour<6){
+        System.out.println("Night!! Time:"+hour+":"+minute+":"+second);
+        }
     }
  
  
